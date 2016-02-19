7e53a7a7903ecd8d47ebe55e48b0b49488154fae
fanshen2010 <1325728530@qq.com>
2016-02-19 15:41:56 -0400

    test

--- proj/pipair_java.java
+++ proj/pipair_java.java
@@ -1,32 +1,29 @@
-package Testcase_Proj;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class pipair {
+public class pipair_java {
 	
     public static void main(String[] args) throws Exception{  
-    	//String[] cmds = {"/bin/sh","-c","ps -ef|grep java"};
-    	
     	int support=3;
     	int confidence=60;
-    	String[] cmds = {"opt -print-callgraph "};
+    	String[] cmds = {"/usr/local/bin/opt","-print-callgraph",""};
     	int argsLength = args.length;
-    	if(argsLength>=2){
-    		System.out.println("filename:"+args[1]);
-    		cmds[0] = cmds[0] + args[1];
-    		if(argsLength>=3){
-    			support = Integer.parseInt(args[2]);
-    			System.out.println("support:"+support);
-    			if(argsLength>=4){
-    				confidence = Integer.parseInt(args[3]);
-    				System.out.println("confidence:"+confidence);
-    			}
-    		}
-    	}
+    	if(argsLength>=1){
+    		System.out.println("filename:"+args[0]);
+			cmds[2] = args[0];
+			if(argsLength>=2){
+				support = Integer.parseInt(args[1]);
+				System.out.println("support:"+support);
+				if(argsLength>=3){
+				    confidence = Integer.parseInt(args[2]);
+				    System.out.println("confidence:"+confidence);
+				}
+			}
+		}
+
     	
-    	//String[] cmds = {"ipconfig"};
         Process pro = Runtime.getRuntime().exec(cmds);  
         pro.waitFor();  
         InputStream in = pro.getInputStream();  
