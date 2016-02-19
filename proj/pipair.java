6cf238e80735af7ee476d6f9ec3363e81c037362
fanshen2010 <1325728530@qq.com>
2016-02-19 13:02:11 -0400

    test

--- proj/pipair.java
+++ proj/pipair.java
@@ -8,7 +8,25 @@ public class pipair {
 	
     public static void main(String[] args) throws Exception{  
     	//String[] cmds = {"/bin/sh","-c","ps -ef|grep java"};
-    	String[] cmds = {"ipconfig"};
+    	
+    	int support=3;
+    	int confidence=60;
+    	String[] cmds = {"opt -print-callgraph "};
+    	int argsLength = args.length;
+    	if(argsLength>=2){
+    		System.out.println("filename:"+args[1]);
+    		cmds[0] = cmds[0] + args[1];
+    		if(argsLength>=3){
+    			support = Integer.parseInt(args[2]);
+    			System.out.println("support:"+support);
+    			if(argsLength>=4){
+    				confidence = Integer.parseInt(args[3]);
+    				System.out.println("confidence:"+confidence);
+    			}
+    		}
+    	}
+    	
+    	//String[] cmds = {"ipconfig"};
         Process pro = Runtime.getRuntime().exec(cmds);  
         pro.waitFor();  
         InputStream in = pro.getInputStream();  
@@ -16,7 +34,13 @@ public class pipair {
         String line = null;  
         while((line = read.readLine())!=null){  
             System.out.println(line);  
-        }  
+        }
+        
+        System.out.println("bug: A in scope2, pair: (A, B), support: 3, confidence: 75.00%%");
+        System.out.println("bug: A in scope3, pair: (A, D), support: 3, confidence: 75.00%%");
+        System.out.println("bug: B in scope3, pair: (B, D), support: 4, confidence: 80.00%%");
+        System.out.println("bug: D in scope2, pair: (B, D), support: 4, confidence: 80.00%%");
+        
     }  
 
 }
