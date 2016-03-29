afedf0abc2564410b2acecac441c86dd1284e49a
fanshen2010 <1325728530@qq.com>
2016-03-29 01:31:24 -0300

    test

--- proj/qi/partA/pipair_java.java
+++ proj/qi/partA/pipair_java.java
@@ -1,6 +1,8 @@
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,8 +15,8 @@ public class pipair_java{
 	 * key = SET(A), SET(A,B),....
 	 * value = SET(SCOPE1,SCOPE2),....
 	 */
-	public static Map<Set<String>,Set<String>> location= new HashMap<Set<String>,Set<String>>();
-	public static Set<String> nearByCallees= new HashSet<String>();
+	public static Map<List<String>,Set<String>> location= new HashMap<List<String>,Set<String>>();
+	public static List<String> nearByCallees= new ArrayList<String>();
 	
     public static void main(String[] args) throws Exception{  
     	
@@ -57,8 +59,7 @@ public class pipair_java{
 			}
 		}
 		scanner.close();
-		//System.out.println(map);
-		//System.out.println(location);
+		System.out.println(location);
 		printBug(support,confidence);
     }
     
@@ -66,78 +67,95 @@ public class pipair_java{
     
     public static void addValueToMap(String addValue,String addLocation){
     	
-    	// check same addvalue
-    	if(nearByCallees.contains(addValue)){
-    		return;
-    	}
-    	
     	//----------------------location-------------------------
-    	Set<String> locationMapKeySet = null;
+    	List<String> locationMapKeyList = null;
     	Set<String> locationMapValueSet = null;
     	for(String key:nearByCallees){
-    		// add otherpair
-    		locationMapKeySet = new HashSet<String>();
-    		locationMapKeySet.add(addValue);
-    		locationMapKeySet.add(key);
-    		if(!location.containsKey(locationMapKeySet)){
+    		// add otherpair positive order
+    		locationMapKeyList = new ArrayList<String>();
+    		locationMapKeyList.add(key);
+    		locationMapKeyList.add(addValue);
+    		if(!location.containsKey(locationMapKeyList)){
     			locationMapValueSet = new HashSet<String>();
     		}else{
-    			locationMapValueSet = location.get(locationMapKeySet);
+    			locationMapValueSet = location.get(locationMapKeyList);
     		}
     		locationMapValueSet.add(addLocation);
-    		location.put(locationMapKeySet, locationMapValueSet);
+    		location.put(locationMapKeyList, locationMapValueSet);
     		
+    		// add otherpair negative order
+    		locationMapKeyList = new ArrayList<String>();
+    		locationMapKeyList.add(addValue);
+    		locationMapKeyList.add(key);
+    		if(!location.containsKey(locationMapKeyList)){
+    			locationMapValueSet = new HashSet<String>();
+    		}else{
+    			locationMapValueSet = location.get(locationMapKeyList);
+    		}
+    		locationMapValueSet.add(addLocation);
+    		location.put(locationMapKeyList, locationMapValueSet);
+    		
+    		// add otherpair negative order
+    		locationMapKeyList = new ArrayList<String>();
+    		locationMapKeyList.add("set");
+			if(key.compareTo(addValue)<0){
+				locationMapKeyList.add(key);
+				locationMapKeyList.add(addValue);
+			}else{
+				locationMapKeyList.add(addValue);
+				locationMapKeyList.add(key);
+			}
+    		if(!location.containsKey(locationMapKeyList)){
+    			locationMapValueSet = new HashSet<String>();
+    		}else{
+    			locationMapValueSet = location.get(locationMapKeyList);
+    		}
+    		locationMapValueSet.add(addLocation);
+    		location.put(locationMapKeyList, locationMapValueSet);
     	}
     	
     	// add selfpair
-		locationMapKeySet = new HashSet<String>();
-		locationMapKeySet.add(addValue);
-		if(!location.containsKey(locationMapKeySet)){
+		locationMapKeyList = new ArrayList<String>();
+		locationMapKeyList.add(addValue);
+		if(!location.containsKey(locationMapKeyList)){
 			locationMapValueSet = new HashSet<String>();
 		}else{
-			locationMapValueSet = location.get(locationMapKeySet);
+			locationMapValueSet = location.get(locationMapKeyList);
 		}
 		locationMapValueSet.add(addLocation);
-		location.put(locationMapKeySet, locationMapValueSet);
+		location.put(locationMapKeyList, locationMapValueSet);
 		
     	return;
     }
-    
-    
-    
     
     
     public static void printBug(int support,double confidence){
     	
     	Integer supportKey = 0;
 		Integer supportPair = 0;
+		Integer supportPairAll = 0;
 		double confidencePair = 0;
+		double confidencePairAll = 0;
 		String singleKeyStr =null;
-		String pairStr1 =null;
-		String pairStr2 = null;
+		String pairStr =null;
+		String pairStr2 =null;
 		
-    	for(Set<String> singleKey:location.keySet()){
+    	for(List<String> singleKey:location.keySet()){
     		if(singleKey.size()==1){
     			supportKey = location.get(singleKey).size();
     			// bugsingle
-				singleKeyStr = singleKey.toArray()[0].toString();
+				singleKeyStr = singleKey.get(0);
 				
-    			for(Set<String> pipairKey:location.keySet()){
+    			for(List<String> pipairKey:location.keySet()){
+    				// findbugpair single should be pipair
         			if(pipairKey.size()>1 && pipairKey.contains(singleKeyStr)){
         				supportPair = location.get(pipairKey).size();
         				confidencePair = (double)supportPair/(double)supportKey;
-        				//System.out.println(singleKey+","+pipairKey+","+supportKey+","+supportPair+","+confidencePair);
+        				System.out.println(singleKey+","+pipairKey+","+supportKey+","+supportPair+","+confidencePair);
         				if(supportKey>=support && supportPair>=support && confidencePair<1 && confidencePair>=confidence){
         					
         					// bugpair
-            				pairStr1 = pipairKey.toArray()[0].toString();
-            				pairStr2 = pipairKey.toArray()[1].toString();
-            				if(pairStr1.compareTo(pairStr2)<0){
-            					pairStr1 = "("+pairStr1+", "+pairStr2+")";
-            				}else{
-            					pairStr1 = "("+pairStr2+", "+pairStr1+")";
-            				}
-            				            				
+            				pairStr = "("+pipairKey.get(0)+", "+pipairKey.get(1)+")";
             				// buglocation
             				Set<String> locationTemp = new HashSet<String>();
             				String locationTempStr =null;
@@ -146,10 +164,43 @@ public class pipair_java{
             				for(String locationTempElem:locationTemp){
             					locationTempStr = locationTempElem;
             					// printbug
-                				System.out.println("bug: "+singleKeyStr+" in "+locationTempStr+", pair: "+pairStr1+", support: "+supportPair+", confidence: "+String.format("%.2f", confidencePair*100)+"%");
+                				System.out.println("bug: "+singleKeyStr+" in "+locationTempStr+", pair: "+pairStr+", support: "+supportPair+", confidence: "+String.format("%.2f", confidencePair*100)+"%");
             				}
             			}
         			}
+        				
+    				// findbugpair pipair should be another pipair
+    				List<String> pipairKey2 = new ArrayList<String>();
+    				Set<String> pipairValue = new HashSet<String>();
+    				Set<String> pipairValue2 = new HashSet<String>();
+    				pipairKey2.add(pipairKey.get(1));
+    				pipairKey2.add(pipairKey.get(0));
+    				pipairValue = location.get(pipairKey);
+    				pipairValue2 = location.get(pipairKey);
+    				if(pipairValue2!=null && !pipairValue2.isEmpty()){
+    					Set<String> pipairlocation = new HashSet<String>();
+    					pipairlocation.addAll(pipairValue);
+    					pipairlocation.addAll(pipairValue2);
+    					supportPairAll = pipairlocation.size();
+    					confidencePairAll = (double)supportPair/(double)supportPairAll;
+						System.out.println(pipairKey+","+pipairKey2+","+supportPair+","+supportPairAll+","+confidencePairAll);
+    					if(supportPair>=support && supportPairAll>=support && confidencePairAll<1 && confidencePairAll>=confidence){
+    						// bugpair
+            				pairStr = "("+pipairKey.get(0)+", "+pipairKey.get(1)+")";
+            				pairStr2 = "("+pipairKey2.get(0)+", "+pipairKey2.get(1)+")";
+            				// buglocation
+            				Set<String> locationTemp = new HashSet<String>();
+            				String locationTempStr =null;
+            				locationTemp.addAll(pipairlocation);
+            				locationTemp.removeAll(pipairValue);
+            				for(String locationTempElem:locationTemp){
+            					locationTempStr = locationTempElem;
+            					// printbug
+                				System.out.println("bug: "+pairStr+" in "+locationTempStr+", pair: "+pairStr2+", support: "+pairStr2+", confidence: "+String.format("%.2f", confidencePair*100)+"%");
+            				}
+        				}
+    				}
+
         		}
     		}
     	}
